6b8201087512a18885cd57a93e1636dd91cdeb15
JProd <rodriguezjoshuapaul@gmail.com>
2017-11-27 01:22:15 -0700

    Edit documentation

--- music.py
+++ music.py
@@ -1,26 +1,30 @@
+#!/usr/bin/env python
+"""
+Imports a given user's LastFM library data into MongoDB.
+"""
 import json
-import urllib2
+import urllib.request
 import pymongo
 
+username = input("Enter Username: ")
+
+# Establishes connection to Mongo
 connection = pymongo.MongoClient("mongodb://localhost")
 
+# Establishes connection to MongoDB collection
+database = connection.lastfm
+artists = database.artists
+# Clears it of precious data
+artists.drop()
 
-# get a handle to the reddit database
-db = connection.reddit
-stories = db.stories
+# Opens LastFM API to access user's library data
+userLibrary = urllib.request.urlopen("http://ws.audioscrobbler.com"
+    "/2.0/?method=library.getartists"
+    "&api_key=66f0acb6a00de7fd814cd490db7e86cf&user={:s}&format=json"
+    .format(username))
 
-# drop existing collection
-stories.drop()
+libraryDictionary = json.loads(userLibrary.read())
 
-# get the reddit home page
-reddit_page = urllib2.urlopen("http://www.reddit.com/r/technology/.json")
-
-# parse the json into python objects
-parsed = json.loads(reddit_page.read())
-
-# iterate through every news item on the page
-for item in parsed['data']['children']:
-    # put it in mongo
-    stories.insert_one(item['data'])
-
-http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=66f0acb6a00de7fd814cd490db7e86cf&user=thepollbox&format=json
+# Iterates though dictionary addidng each artist as a document
+for item in libraryDictionary['artists']['artist']:
+    artists.insert_one(item)
